43bec93ff769c2d61be60c55f8a72c763d7bb87c
Jelte Dirks <j.dirks@outlook.com>
2022-11-26 18:36:17 +0100

    some more prac?
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- helloworld.c
+++ helloworld.c
@@ -2,15 +2,22 @@
 #include <unistd.h>  // Needed to output to the world
 
 // define the parameter list and the body yourself...
-static void add_and_multi_with_4_parameters (/* TODO */)
+static void add_and_multi_with_4_parameters (int *a, int *b, int *c, int *d)
 {
-    // TODO
+  int new_c = *a + *b;
+  int new_d = *a * *b;
+  *c = new_c;
+  *d = new_d;
 }
 
 // define the parameter list and the body yourself...
-static void add_and_multi_with_2_parameters (/* TODO */)
+static void add_and_multi_with_2_parameters (int *a, int *b)
 {
-    // TODO
+  int new_a = *a + *b;
+  int new_b = *a * *b;
+  
+  *a = new_a;
+  *b = new_b;
 }
 
 // The main program function
@@ -20,8 +27,6 @@ int main(void)
     int b = -5;
     int c;
     int d;
-
-    sleep(10);
 
     for (int z = 0; z < a; z++)
     {
@@ -35,15 +40,15 @@ int main(void)
       
       // task 1: call function add_with_4_parameters() to realize the postcodition: c=a+b and d=a*b
       // (remove the comments of the next 2 lines)
-      // add_and_multi_with_4_parameters ( <your_parameter_list> )
-      // printf ("a=%d b=%d c=%d d=%d\n", a, b, c, d);
+      add_and_multi_with_4_parameters (&a, &b, &c, &d);
+      printf ("a=%d b=%d c=%d d=%d\n", a, b, c, d);
       
       // tasks to work with pointers:
       // task 2: call function add_with_2_parameters() to realize the postcodition: a=a+b and b=a*b
       //         (read them as: "new-a becomes old-a + old-b" and "new-b becomes old-a * old-b"
       // (remove the comments of the next 2 lines)
-      // add_and_multi_with_2_parameters ( <your_parameter_list> )
-      // printf ("a=%d b=%d\n", a, b);
+      add_and_multi_with_2_parameters (&a, &b);
+      printf ("a=%d b=%d\n", a, b);
       
       // Successful termination
       
--- main.c
+++ main.c
@@ -1,11 +1,98 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
+int c_structs() {
+  struct Student {
+    char name[100];
+    char major[100];
+    int s_id;
+  };
+
+  struct Student jea;
+  jea.s_id = 491824;
+  strcpy(jea.name, "Deez nuts");
+  strcpy(jea.major, "Too easy");
+
+  return 0;
+}
+
+int arrays_and_stuff() {
+  int list_of_stuff[] = { 1, 2, 5, 6236326, 6};
+  printf("%d\n", list_of_stuff[4]);
+  return 0;
+}
+
+int user_input() {
+  char some_text[20];
+  printf("Give me text:");
+  scanf("%s", some_text);
+  printf("Thanks for %s\n", some_text);
+
+  int digit;
+  printf("Give me a digit:");
+  scanf("%d", &digit);
+  printf("Thanks for %d\n", digit);
+
+  // for text with spaces
+  fgets(some_text, 20, stdin);
+  printf("%s\n", some_text);
+
+  return 0;
+}
+
+int numbers_in_c() {
+  printf("%f\n", 4 + 5.2);
+  printf("%f\n", 4 - 5.2);
+  printf("%f\n", 4 * 5.2);
+  printf("%f\n", 4 / 5.2);
+  printf("%d\n", 4 / 3); // 1 
+  printf("%d\n", 4.5); // 73896
+  return 0;
+}
+
+int printf_stuff() {
+  printf("First line\n");
+  printf("Format specifiers %d\n", 10);
+  printf("Format specifiers %s", "what?\n");
+  printf("Format specifiers %f\n", 4.51025);
+  printf("%c\n", 'D');
+
+  return 0;
+}
+
+int data_types() {
+  /*
+   * Basic data types
+   */
+  int age = 27;
+  double weight = 74.5;
+  char initial = 'J';
+
+  char sentence[] = "A string of characters";
+
+  return 0;
+}
+
+int variables() {
   int age = 30;
   char name[] = "DEEZ NUTS";
 
   printf("There were some nuts: %s\n", name);
   printf("The nuts were %d years old\n", age);
 
+  const int AGE = 25; // wouldn't that be nice
+
   return 0;
 }
+
+int main() {
+  variables();
+  data_types();
+  printf_stuff();
+  numbers_in_c();
+  user_input();
+  arrays_and_stuff();
+  c_structs();
+  return 0;
+}
+
